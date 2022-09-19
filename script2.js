7dbfa26ced0d7b2d768fdf4044661ded6719639b
r-sailokesh <84587708+r-sailokesh@users.noreply.github.com>
2022-09-19 11:00:22 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -10,11 +10,11 @@ function validate() {
     username.charCodeAt(0) > 90
   ) {
     alert(
-      "Username or Password can't be blank or Username must have more than 8 digits."
+      "Username or Password can't be blank or Username must have more than 8 digits or Username must start with a capital letter."
     );
     return false;
   } else if (password.length < 6 || password.search(/[@#$%]/) == -1) {
-    alert("Password must be at least 6 characters long.");
+    alert("Password must be at least 6 characters long and must cosist of atleast one special character.");
     return false;
   }
   return true;
